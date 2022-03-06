7281723895ec2824a19dbedfdf58f1e3d57bfab4
Vincent McGargill <vincent@fixedopsdigital.com>
2022-03-06 16:12:39 -0700

    Trying php artisan migrate --force command instead.

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+php artisan migrate --force
+
 sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
 php-fpm -D
 
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
-
-php artisan migrate:fresh
 
 nginx
