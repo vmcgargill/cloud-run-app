ab7e92f29aa98db79af5afa06b19878b1959fba8
Vincent McGargill <vincent@fixedopsdigital.com>
2022-03-06 16:44:26 -0700

    Adding a fea breaks.

--- src/resources/views/livewire/note-form.blade.php
+++ src/resources/views/livewire/note-form.blade.php
@@ -10,7 +10,7 @@
         <button type="button" wire:click.prevent="submit" style="padding: 10px; border-radius: 5px; cursor: pointer;">
             Submit
         </button>
-    </div>
+    </div><br><br>
     <div class="mt-5">
         <table>
             @foreach($notes as $n)
